d98a99311f1a84066d0803489a3320997f1aa2ba
winnie <winnie_fon@hotmail.com>
2017-10-12 23:47:36 -0400

    reverse linked list

---
+++ reverseList.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
+
+#for the recursive way
+# class Solution:
+    # def reverseList(self, head):
+        # return self._reverse(head)
+    # def _reverse(self, node, prev=None):
+        # if not node:
+            # return prev
+        # n = node.next
+        # node.next = prev
+        # return self._reverse(n, node)
